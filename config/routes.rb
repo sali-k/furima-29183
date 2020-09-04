fc36507853fb234c08166513a11988c824882ca4
sali-k <saki.kimura78@gmail.com>
2020-09-04 21:09:37 +0900

    ログアウト機能(修正)
    This reverts commit 2b36b48dd1d0a4644a40ad1c664cba0c91986169.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items
-
-  # devise_scope :user do
-  #   get '/users/sign_out' => 'devise/sessions#destroy'
-  # end
-
 end

0943a95c42f7b6d0a54a3f899a0bb7e547ca6ef1
sali-k <saki.kimura78@gmail.com>
2020-09-04 15:48:00 +0900

    passwordカラム削除

--- db/migrate/20200903070505_devise_create_users.rb
+++ db/migrate/20200903070505_devise_create_users.rb
@@ -6,7 +6,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
-      t.string :password,           null: false
       t.string :family_name,        null: false
       t.string :first_name,         null: false
       t.string :family_name_kana,   null: false

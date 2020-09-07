8ef96da99ba010d72c8f58a325521683e08997d6
sali-k <saki.kimura78@gmail.com>
2020-09-07 11:03:22 +0900

    Itemモデルのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
-  
+  has_one :purchase
+  belongs_to :user
+  has_one_attached :image
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage_payer
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :handing_time
 end

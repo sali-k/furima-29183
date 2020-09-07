2db8c246bee31f9d3d8d38c2c6f7ac318dd751d3
sali-k <saki.kimura78@gmail.com>
2020-09-07 16:13:30 +0900

    itemモデルにバリデーション記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,23 @@ class Item < ApplicationRecord
   has_one :purchase
   belongs_to :user
   has_one_attached :image
-  belongs_to_active_hash :category
-  belongs_to_active_hash :condition
-  belongs_to_active_hash :postage_payer
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :handing_time
+  #belongs_to_active_hash :category
+  #belongs_to_active_hash :condition
+  #belongs_to_active_hash :postage_payer
+  #belongs_to_active_hash :prefecture
+  #belongs_to_active_hash :handing_time
+  
+  validates :image, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
+
+  with_options presence: true do
+    validates :category_id
+    validates :condition_id
+    validates :postage_payer_id
+    validates :prefecture_id
+    validates :handing_time_id
+  end
+
 end

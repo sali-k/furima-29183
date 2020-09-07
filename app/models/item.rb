7e46da42dba43f88f1f2940705f4c99bc5088f9f
sali-k <saki.kimura78@gmail.com>
2020-09-07 21:22:54 +0900

    コンディション選択実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   belongs_to_active_hash :category
-  #belongs_to_active_hash :condition
+  belongs_to_active_hash :condition
   #belongs_to_active_hash :postage_payer
   #belongs_to_active_hash :prefecture
   #belongs_to_active_hash :handing_time
@@ -14,9 +14,9 @@ class Item < ApplicationRecord
   validates :description, presence: true, length: { maximum: 1000 }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
   validates :category_id, presence: true, numericality: { other_than: 1 } 
+  validates :condition_id, presence: true, numericality: { other_than: 1 } 
 
   with_options presence: true do
-    validates :condition_id
     validates :postage_payer_id
     validates :prefecture_id
     validates :handing_time_id

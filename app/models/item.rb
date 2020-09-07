46fc9865bebbeabb544a466d7078e39a5aec2775
sali-k <saki.kimura78@gmail.com>
2020-09-07 19:35:53 +0900

    カテゴリー機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   has_one :purchase
   belongs_to :user
   has_one_attached :image
-  #belongs_to_active_hash :category
+  belongs_to_active_hash :category
   #belongs_to_active_hash :condition
   #belongs_to_active_hash :postage_payer
   #belongs_to_active_hash :prefecture
@@ -12,9 +13,9 @@ class Item < ApplicationRecord
   validates :name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
+  validates :category_id, presence: true, numericality: { other_than: 1 } 
 
   with_options presence: true do
-    validates :category_id
     validates :condition_id
     validates :postage_payer_id
     validates :prefecture_id

ed56ee23ff9a1d20e3c04c5d046b208dcb0728e1
sali-k <saki.kimura78@gmail.com>
2020-09-07 09:42:16 +0900

    Itemコントローラー追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,15 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :handing_time_id, :price)
   end
 end

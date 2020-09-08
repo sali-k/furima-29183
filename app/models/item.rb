f0988251abee6a5b2b00e5e097024438ab0011d4
sali-k <saki.kimura78@gmail.com>
2020-09-08 19:03:18 +0900

    with_optionsへの書き換え

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,14 +9,15 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :handing_time
 
-  validates :image, presence: true
-  validates :name, presence: true, length: { maximum: 40 }
-  validates :description, presence: true, length: { maximum: 1000 }
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
-
-  validates :category_id, presence: true, numericality: { other_than: 1 }
-  validates :condition_id, presence: true, numericality: { other_than: 1 }
-  validates :postage_payer_id, presence: true, numericality: { other_than: 1 }
-  validates :prefecture_id, presence: true, numericality: { other_than: 0 }
-  validates :handing_time_id, presence: true, numericality: { other_than: 1 }
+  with_options presence: true do
+    validates :image
+    validates :name, length: { maximum: 40 }
+    validates :description, length: { maximum: 1000 }
+    validates :category_id, numericality: { other_than: 1 }
+    validates :condition_id, numericality: { other_than: 1 }
+    validates :postage_payer_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :handing_time_id, numericality: { other_than: 1 }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
+  end
 end

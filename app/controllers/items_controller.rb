bb5a98844c031933441eaabe09d80b916954f8e3
sali-k <saki.kimura78@gmail.com>
2020-09-09 15:16:14 +0900

    Rubocop済

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all.order('created_at DESC')
   end
-  
+
   def new
     @item = Item.new
   end
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
 
   def show
   end
-  
+
   private
 
   def item_params

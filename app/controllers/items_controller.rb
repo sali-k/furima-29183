3d7541574e460eb5ac8e2d2ebd4a4757bb494457
sali-k <saki.kimura78@gmail.com>
2020-09-11 17:29:44 +0900

    Revert "Revert "削除機能の実装、Rubocop済""
    This reverts commit c6fb6e7e4a60cc6fe10fc1538cdd4fa08e4150bd.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -31,6 +31,16 @@ class ItemsController < ApplicationController
       redirect_to item_path(@item)
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if user_signed_in? && current_user.id == @item.user.id
+      if @item.destroy
+        redirect_to root_path
+      else
+        render :show
+      end
     end
   end
 

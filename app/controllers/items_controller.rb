e1e1fb687047e1c079c6e00cf9f46d4c279752c9
sali-k <saki.kimura78@gmail.com>
2020-09-11 14:57:52 +0900

    ログインしているユーザーのidが出品者のidと一致しなければroot_pathへリダイレクト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if user_signed_in? && current_user.id != @item.user.id
+      redirect_to root_path
+    end
   end
 
   def update

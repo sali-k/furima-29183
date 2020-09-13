b6b22458c530f4d668fef72278a4aa913f81e2d5
sali-k <saki.kimura78@gmail.com>
2020-09-14 08:44:47 +0900

    create 関連の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,8 +9,15 @@ class PurchasesController < ApplicationController
   end
 
   def new
+    @purchase = Purchase.new
   end
 
- def create
- end
+  def create
+    @purchase = Purchase.create(purchase_params)
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase).permit(:post_code, :prefectures_id, :city, :home_number, :building_name, :phone_number, :name_reading, :purchase).merge(user_id: current_user.id)
+  end
 end

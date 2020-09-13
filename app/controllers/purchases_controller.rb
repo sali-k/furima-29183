ad5a90541075d59b68fd813f1eab59d3b6a9e988
sali-k <saki.kimura78@gmail.com>
2020-09-13 19:41:14 +0900

    購入済商品へ直接遷移できない制約

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,7 +3,7 @@ class PurchasesController < ApplicationController
   
   def index
     @item = Item.find(params[:item_id])
-    if current_user.id == @item.user.id
+    if current_user.id == @item.user.id || @item.purchase != nil
       redirect_to root_path
     end
   end

25c7ae8fe784e9a0080faace40a8e2f58d721ed1
sali-k <saki.kimura78@gmail.com>
2020-09-13 17:34:18 +0900

    郵便番号と電話番号の制約

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,11 +3,11 @@ class Address < ApplicationRecord
   belongs_to_active_hash :prefecture
 
   with_options presence: true do
-    validates :post_code
+    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'は「-」を含めて入力してください' }
     validates :prefectures_id
     validates :city 
     validates :home_number
-    validates :phone_number
-    validates :purchase 
+    validates :phone_number, format: { with: /\A\d{11}\z/ , message: 'は「-」を除いて入力してください' }
+    validates :purchase
   end
 end

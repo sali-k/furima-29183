30b378fec39cb3671051b87410c44d4372c1bc6b
sali-k <saki.kimura78@gmail.com>
2020-09-13 19:33:27 +0900

    未ログインと出品者自身の購入への制約

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,11 @@
 class PurchasesController < ApplicationController
-
+  before_action :authenticate_user!
+  
   def index
     @item = Item.find(params[:item_id])
+    if current_user.id == @item.user.id
+      redirect_to root_path
+    end
   end
 
   def new

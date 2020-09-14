1661d163d174bb51f6fb3bd3b7da0a3b100018e9
sali-k <saki.kimura78@gmail.com>
2020-09-14 20:51:26 +0900

    PurchaseAddress RSpec 作成

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,44 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
-  describe '商品購入機能' do
-    before do
-      @Purchase = FactoryBot.build(:purchase)
-      #@item.image = fixture_file_upload('spec/test_image.png')
-    end
-
-    it "必要事項が入力されていれば購入できる" do
-      #expect(@purchase).to be_valid
-    end
-
-    it "tokenが空では登録できない" do
-    end
-
-    it "post_codeが空では登録できない" do
-    end
-
-    it "post_codeに「-」が含まれていないと登録できない" do
-    end
-
-    it "post_codeが8文字以上だと登録できない" do
-    end
-
-    it "prefectures_idが空では登録できない" do
-    end
-
-    it "cityが空では登録できない" do
-    end
-
-    it "home_numberが空では登録できない" do
-    end
-
-    it "phone_numberが空では登録できない" do
-    end
-
-    it "phone_numberに「-」が含まれていると登録できない" do
-    end
-
-    it "phone_numberが12以上だと登録できない" do
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
